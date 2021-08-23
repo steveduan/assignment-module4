e292d3213576ff445771af2279b3273476a3d488
dogger <15752815597@163.com>
2021-08-23 15:12:54 +0800

    change

--- src/menu/categories.component.js
+++ src/menu/categories.component.js
@@ -3,7 +3,8 @@
     
     angular.module('MenuApp')
     .component('categoriesComponent', {
-      templateUrl: '/src/menu/template/categories.list.html',
+      // templateUrl: '/src/menu/template/categories.list.html',
+      template:"<ul><li ng-repeat='item in $ctrl.items' ui-sref='mainCategories.itemDetail({itemId: $index})'>{{item.name}}{{item.short_name}}</li></ul>",
       bindings: {
         items: '<'
       }
